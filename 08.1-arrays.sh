461cdf7ef3a02de83e1906f9dfcbf736c82ee4b1
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 22:17:10 +0530

    arrays in the script

---
+++ 08.1-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Array
+FRUITS=("APPLE" "BANANA" "mango")
+
+echo "FRUITS are: ${FRUITS[@]}"
+echo "first FRUITS is: ${FRUITS[0]}"
+echo "second FRUITS is: ${FRUITS[1]}"
+echo "third FRUITS is: ${FRUITS[2]}"
