7ce28e33ecb94062c48d291364e48851572f60f5
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 15:29:28 +0530

    starting variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERSON1=Ram
 PERSON2=Kumar
-echo "$PERSON1: hello chintu how are you"
-echo "$PERSON2: hi prabhu, i am fine thanks for asking, how are you doing"
+echo "$PERSON1: hello $PERSON2 how are you"
+echo "$PERSON2: hi $PERSON1, i am fine thanks for asking, how are you doing"
 echo "$PERSON1: doing fine, what are you learning $PERSON2"
 echo "$PERSON2: i am learning shell, what about you"
