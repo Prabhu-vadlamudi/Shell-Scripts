e0e37ba52a983bed75642c425e99069653a5c88f
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 16:02:34 +0530

    executing commands dynamically to store in the variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-START_TIME=$(date +%S)
-END_TIME=$(date +%S)
+START_TIME=$(date +%s)
+
 echo "Script execution start time: $START_TIME"
 
-sleep 20
-
+sleep 10
+END_TIME=$(date +%s)
 echo "Script execution end time: $END_TIME"
 TOTAL_TIME=$((END_TIME-START_TIME))
 echo "Duration for execution of the script $TOTAL_TIME"
