3a67b0fc5721f3a739c142d13086c7da57743a1e
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 15:17:41 +0530

    changed something

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Prabhu: hello chintu how are you"
+echo "chintu: hi prabhu, i am fine thanks for asking, how are you doing"
+echo "prabhu: doing fine, what are you learning chintu"
+echo "chintu: i am learning shell, what about you"
