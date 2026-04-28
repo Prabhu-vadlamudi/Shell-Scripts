020c49340c195a98b16722c5cb791ea324a9ef2e
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 22:59:52 +0530

    verifing previous command is success or failed with exit codes  simple if condition

---
+++ 10.3-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+echo "Please run the script with root user access"
+exit 1
+fi
+
+echo "Installing nginx"
+dnf install nginnnx -y
+
+if [ $? -ne 0 ]; then
+echo "installing nginx ... failure"
+else
+echo "installing nginx ...success"
+fi
