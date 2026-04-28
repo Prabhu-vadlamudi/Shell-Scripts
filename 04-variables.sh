7502748a6f5479bdb63b45bddda6afd34bb9c186
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 15:37:54 +0530

    command line arguments passing in variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,4 +3,4 @@
 echo "$1: hello $2 how are you"
 echo "$2: hi $1, i am fine thanks for asking, how are you doing"
 echo "$1: doing fine, what are you learning $2"
-echo "$2: i am learning shell, what about you"
+echo "$2: i am learning shell, what about you $1"
