f3293b755f97049401cc96deb402e0c1e37467bf
prabhudevops-dev <prabhu6vadlamudi@gmail.com>
2026-04-28 15:33:35 +0530

    starting variables

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "$1: hello $2 how are you"
+echo "$2: hi $1, i am fine thanks for asking, how are you doing"
+echo "$1: doing fine, what are you learning $2"
+echo "$2: i am learning shell, what about you"
